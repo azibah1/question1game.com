0c715f24fa8db02f8f0cc36fb9a0beb620ec3a07
azibah1 <108614384+azibah1@users.noreply.github.com>
2023-07-20 20:59:56 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,141 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "What is the capital city of Liberia?",
+    answer: "Monrovia",
+    options: ["Monrovia", "Oslo", "Caro", "Accra"],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful St yle Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor",
+    ],
+  },
+  {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language",
+    ],
+  },
+  {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language",
+    ],
+  },
+  {
+    numb: 6,
+    question: "The word Walking is which part of speech?",
+    answer: "Verb",
+    options: ["Adjective", "Pronoun", "Verb", "Conjunction"],
+  },
+  {
+    numb: 7,
+    question: "Who is the father of Computer?",
+    answer: "Charles Babbage",
+    options: [
+      "Charles Babbage",
+      "Albert Einstein",
+      "Galileo Galilei",
+      "Isaac Newton",
+    ],
+  },
+  {
+    numb: 8,
+    question: "OS computer abbreviation usually means",
+    answer: "Operating System",
+    options: [
+      "Order Of Significance",
+      "Open Software ",
+      "Operating System",
+      "Optical Sensor",
+    ],
+  },
+  {
+    numb: 9,
+    question: "Mov extension refers usually to what kind of files?",
+    answer: "Animation/Movie file",
+    options: [
+      "Animation/Movie file",
+      "Audio file",
+      "MS office document",
+      "Image file",
+    ],
+  },
+  {
+    numb: 10,
+    question: "Which Continent is the largest?",
+    answer: "Asia",
+    options: ["Europe", "Asia", "Africa", "Australia"],
+  },
+  {
+    numb: 11,
+    question: "Liberai is in which continent?",
+    answer: "Africa",
+    options: ["Africa", "Europe", "Asia ", "America"],
+  },
+  {
+    numb: 12,
+    question: "What is the capital city of Turkey?",
+    answer: "Ankara",
+    options: ["Instabul", "Famagusa", "Dakar", "Ankara"],
+  },
+
+  {
+    numb: 13,
+    question: "Who is the President of America ?",
+    answer: "Joe Biden",
+    options: ["George M. Weah", "George Bush", "Joe Biden", "Isaac Newton"],
+  },
+  {
+    numb: 14,
+    question: "Who invented Javascript?",
+    answer: "Brendan Eich",
+    options: [
+      "Charles Babbage",
+      "Brenda Eich",
+      "Galileo Galilei",
+      "Isaac Newton",
+    ],
+  },
+  {
+    numb: 15,
+    question: "USB Stand for",
+    answer: "Universal Serial Bus",
+    options: [
+      "uniform System Broadcasting",
+      "United Steel Band",
+      "Universal Serial Bus",
+      "Universa System Bus",
+    ],
+  },
+
+  // },
+];
